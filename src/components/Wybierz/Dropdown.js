3f0b29b377baf8d1616b0ebd41e2d7ee9384ef35
orzeelekk <orzellpawell@gmail.com>
2025-01-28 12:23:44 +0100

    korekty opisowe i header i footer

--- src/components/Wybierz/Dropdown.js
+++ src/components/Wybierz/Dropdown.js
@@ -1,24 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-
-const Dropdown = ({ submenus,dropdown }) => {
-    console.log(submenus)
-
+const Dropdown = ({ submenus, dropdown }) => {
     return (
         <ul className={`dropdown ${dropdown ? "show" : ""}`}>
             {submenus.map((submenu, index) => (
                 <ul key={index} className="menu-items">
-                    <li>Powierzchnia terenu: {submenu.powierzchnia_terenu}</li>
-                    <li>Powierzchnia zabudowy: {submenu.powierzchnia_zabudowy}</li>
-                    <li>Powierzchnia użytkowa: {submenu.powierzchnia_uzytkowa}</li>
-                    <li>Kubatura: {submenu.kubatura}</li>
-                    <li>Ściany nośne zewnętrzne i wewnętrzne: {submenu.ściany_nośne_zewnętrzne_i_wewnętrzne}</li>
-                    <li>Dach: {submenu.dach}</li>
-                    <li>Instalacja grzewcza: {submenu.instalacja_grzewcza}</li>
-                    <li>Instalacja wentylacyjna: {submenu.instalacja_wentylacyjna}</li>
-                    <li>Ogrodzenie: {submenu.ogrodzenie}</li>
-                    <li>Oprograowanie: {submenu.oprogramowanie}</li>
+                    {Object.entries(submenu).map(([key, value]) => (
+                        <li key={key}>
+                            <span className="bold">{key.replaceAll('_', ' ')}:</span> <span className="normal">{value}</span>
+                        </li>
+                    ))}
                 </ul>
             ))}
         </ul>
@@ -32,16 +24,15 @@ Dropdown.propTypes = {
             powierzchnia_zabudowy: PropTypes.string.isRequired,
             powierzchnia_uzytkowa: PropTypes.string.isRequired,
             kubatura: PropTypes.string.isRequired,
-            zabudowa: PropTypes.string.isRequired,
             ściany_nośne_zewnętrzne_i_wewnętrzne: PropTypes.string.isRequired,
+            dach: PropTypes.string.isRequired,
             instalacja_grzewcza: PropTypes.string.isRequired,
-            wentylacja: PropTypes.string.isRequired,
+            instalacja_wentylacyjna: PropTypes.string.isRequired,
             ogrodzenie: PropTypes.string.isRequired,
             oprogramowanie: PropTypes.string.isRequired,
         })
-    ).isRequired, // Expect an array of objects
-    dropdown: PropTypes.bool.isRequired, // Expect a boolean
+    ).isRequired,
+    dropdown: PropTypes.bool.isRequired,
 };
 
 export default Dropdown;
-
--- src/components/Wybierz/daneTechniczne.js
+++ src/components/Wybierz/daneTechniczne.js
@@ -3,17 +3,17 @@ export const daneTechniczne = [
         title: 'Dom wolonostojący',
         submenu: [
             {
-                powierzchnia_terenu: '500-580m2',
-                powierzchnia_zabudowy: '99m2',
-                powierzchnia_uzytkowa: '140m2',
-                kubatura: '757m3',
-                zabudowa: 'Dom wolnostojący',
-                ściany_nośne_zewnętrzne_i_wewnętrzne: 'murowane z betonu komórkowego kl. 15MPa na zaprawie cementowej M10.',
-                dach: 'Więzba dachowa drewniana dwuspadowa o kontrukcji krokwiowo-jętkowej z pokryciem blachą na rąbek stojący',
-                instalacja_grzewcza: 'Pompa ciepła z ogrzewaniem podłogowym',
-                instalacja_wentylacyjna: 'System rekuperacji i kominy wentylacyjne',
-                ogrodzenie: 'Front budynku murowany wraz z siatką ogrodzeniową po obwodzie budynku',
-                oprogramowanie: 'Możliwość instalacji systemu smarthouse do zdalnego sterowania poszczególnymi urządzeniami, w tym: bramą garażową, pompą ciepła, rekuperacją, oświetleniem, fotowoltaiką, systemami alarmowymi i innymi podobnymi urządzeniami.'
+                Powierzchnia_terenu: '500-580m2',
+                Powierzchnia_zabudowy: '99m2',
+                Powierzchnia_uzytkowa: '145m2',
+                Kubatura: '757m3',
+                Zabudowa: 'Dom wolnostojący',
+                Ściany_nośne_zewnętrzne_i_wewnętrzne: 'murowane z betonu komórkowego kl. 15MPa na zaprawie klejowej.',
+                Dach: 'Więzba dachowa drewniana dwuspadowa o kontrukcji krokwiowo-jętkowej pokryta blachą w kolorze antracytowym.',
+                Instalacja_grzewcza: 'Pompa ciepła z ogrzewaniem podłogowym',
+                Instalacja_wentylacyjna: 'System rekuperacji.',
+                Ogrodzenie: 'Otwarty front budynku wykonano z pełnego murowanego ogrodzenia, wzdłuż obwodu zastosowano ogrodzenie z paneli siatkowych na podmurówce.',
+                Oprogramowanie: 'Możliwość instalacji systemu smarthouse do zdalnego sterowania poszczególnymi urządzeniami, w tym: bramą garażową, pompą ciepła, rekuperacją, oświetleniem, fotowoltaiką, systemami alarmowymi i innymi podobnymi urządzeniami.'
             }
         ],
     },
@@ -21,17 +21,17 @@ export const daneTechniczne = [
         title: 'Dom wolnostojący typu blizniak (przewidywany w kolejnych etapach realizacji)',
         submenu: [
             {
-                powierzchnia_terenu: '530-550m2',
-                powierzchnia_zabudowy: '80m2',
-                powierzchnia_uzytkowa: '120m2',
-                kubatura: '680m3',
-                zabudowa: 'Dom wolnostojący typu blizniak',
-                ściany_nośne_zewnętrzne_i_wewnętrzne: 'murowane z betonu komórkowego kl. 15MPa na zaprawie cementowej M10.',
-                dach: 'Więzba dachowa drewniana dwuspadowa o kontrukcji krokwiowo-jętkowej z pokryciem blachą na rąbek stojący',
-                instalacja_grzewcza: 'Pompa ciepła z ogrzewaniem podłogowym',
-                wentylacja: 'System rekuperacji i kominy wentylacyjne',
-                ogrodzenie: 'Front budynku murowany wraz z siatką ogrodzeniową po obwodzie budynku',
-                oprogramowanie: 'Możliwość instalacji systemu smarthouse do zdalnego sterowania poszczególnymi urządzeniami, w tym: bramą garażową, pompą ciepła, rekuperacją, oświetleniem, fotowoltaiką, systemami alarmowymi i innymi podobnymi urządzeniami.'
+                Powierzchnia_terenu: '530-550m2',
+                Powierzchnia_zabudowy: '80m2',
+                Powierzchnia_uzytkowa: '120m2',
+                Kubatura: '680m3',
+                Zabudowa: 'Dom wolnostojący',
+                Ściany_nośne_zewnętrzne_i_wewnętrzne: 'murowane z betonu komórkowego kl. 15MPa na zaprawie klejowej.',
+                Dach: 'Więzba dachowa drewniana dwuspadowa o kontrukcji krokwiowo-jętkowej pokryta blachą w kolorze antracytowym.',
+                Instalacja_grzewcza: 'Pompa ciepła z ogrzewaniem podłogowym',
+                Instalacja_wentylacyjna: 'System rekuperacji.',
+                Ogrodzenie: 'Otwarty front budynku wykonano z pełnego murowanego ogrodzenia, wzdłuż obwodu zastosowano ogrodzenie z paneli siatkowych na podmurówce.',
+                Oprogramowanie: 'Możliwość instalacji systemu smarthouse do zdalnego sterowania poszczególnymi urządzeniami, w tym: bramą garażową, pompą ciepła, rekuperacją, oświetleniem, fotowoltaiką, systemami alarmowymi i innymi podobnymi urządzeniami.'
             }
         ],
     },
